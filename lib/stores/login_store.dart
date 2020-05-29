d882fffca1e1c241e1383ac9181e4b4c15e07e94
Samilly Nunes <samillynunes19@gmail.com>
2020-05-29 17:35:35 -0300

    Autorun reaction added, setters actions added to login screen

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -6,6 +6,15 @@ class LoginStore = _LoginStore with _$LoginStore;
 
 abstract class _LoginStore with Store{
 
+  _LoginStore(){
+    autorun( //eh um tipo de reacao que eh chamada sempre que algum observable tem um dado lido ou modificado dentro dela
+      (_){
+        print("Email: "+email); //ja vai printar o primeiro valor que observar, q no caso eh o vazio
+        print(password);
+      }
+    );
+  }
+
   @observable
   String email="";
 
